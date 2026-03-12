ffea786d8b4b850a004259a9c1308faf36bd3885
unknown <nishawaghmare2708@gmail.com>
2026-03-12 17:34:43 +0530

    Calculator UI layout completed

--- App.tsx
+++ App.tsx
@@ -1,44 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+    <SafeAreaView style={styles.container}>
+      {/* <Text style={styles.title}>Calculator App</Text> */}
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+      <View style={styles.display}>
+        <Text style={styles.displayText}> 0 </Text>
+      </View>
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+      <View style={styles.buttons}>
+        {/* <Text> Buttons will come here </Text> */}
+
+        <View style={styles.row}>
+          <Text style={styles.btn}>7</Text>
+          <Text style={styles.btn}>8</Text>
+          <Text style={styles.btn}>9</Text>
+          <Text style={styles.btn}>/</Text>
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.btn}>4</Text>
+          <Text style={styles.btn}>5</Text>
+          <Text style={styles.btn}>6</Text>
+          <Text style={styles.btn}>*</Text>
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.btn}>1</Text>
+          <Text style={styles.btn}>2</Text>
+          <Text style={styles.btn}>3</Text>
+          <Text style={styles.btn}>-</Text>
+        </View>
+
+        <View style={styles.row}>
+          <Text style={styles.btn}>0</Text>
+          <Text style={styles.btn}>.</Text>
+          <Text style={styles.btn}>=</Text>
+          <Text style={styles.btn}>+</Text>
+        </View>
+        
+      </View>
+
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor:'#001',
+  },
+  // title: {
+  //   fontSize: 28,
+  //   fontWeight: 'bold',
+  // },
+  display:{
+    flex:2,
+    justifyContent:'flex-end',
+    alignItems:'flex-end',
+    padding:20,
+  },
+  displayText:{
+    fontSize:48,
+    color:'white',
+  },
+  buttons:{
+    flex:5,
+    backgroundColor:'#1c1c1c',
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  row:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    width:'100%',
+    marginVertical:10,
+  },
+  btn:{
+    fontSize:28,
+    color:'white',
+    backgroundColor:'#333',
+    width:60,
+    height:60,
+    textAlign:'center',
+    textAlignVertical:'center',
+    borderRadius:30,
   },
 });
 
